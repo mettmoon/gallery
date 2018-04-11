555f423eb50967674351dc557651e6fd48588617
문유성 <petermoon@lfcorp.com>
2018-04-11 13:32:26 +0900

    Initial Commit

---
+++ gallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gallery
+//
+//  Created by Peter Moon on 11/04/2018.
+//  Copyright © 2018 Peter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
